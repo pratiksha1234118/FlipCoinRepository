723ed9d30e4dbdd787a17ba05b035df12419b4a6
pratiksha1234118 <pratikshagavande1699@gmail.com>
2020-07-08 12:21:27 +0530

    modified :Doublet combination is added

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
-heads=0
-tails=0
+hh=0
+tt=0
+ht=0
+th=0
 read -p "enter the  number of tym you want to flip a coin" num
-declare -A coin
-heads=0
-tails=0
 
+#uc4
+declare -A coin
 for((i=1;i<num;i++))
 do
-        no=$((RANDOM%2))
+         no=$((RANDOM%4))
         if [ $no -eq 1 ]
         then
-                combination=H
-                (( heads ++ ))
+        (( hh  ++ ))
+        combination=HH
+        elif [ $no -eq 2 ]
+        then
+        (( tt  ++ ))
+        combination=TT
+        elif [ $no -eq 3 ]
+        then
+        (( ht  ++ ))
+        combination=HT
         else
-                 combination=T
-                (( tails++ ))
-          fi
-        coin[$i]=$combination
+        (( th  ++ ))
+        combination=TH
+        fi
+        coin[$i]="$combination"
 done
-echo ${coin[@]}
-echo "percentage of heads: " $((( heads * 100 ) / num ))"%"
-echo "percentage of tails: " $((( tails * 100 ) / num ))"%"
+echo "Dictionary : ${coin[@]}"
+echo "percentage of HH: " $((( hh * 100 ) / num ))"%"
+echo "percentage of TT: " $((( tt * 100 ) / num ))"%"
+echo "percentage of HT: " $((( ht * 100 ) / num ))"%"
+echo "percentage of TH: " $((( th * 100 ) / num ))"%"
 
 
 
