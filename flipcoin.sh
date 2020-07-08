b53eaa5d6f07b9672008e25c1d0e48a99cce3a3e
pratiksha1234118 <pratikshagavande1699@gmail.com>
2020-07-08 12:27:50 +0530

    modified :Triplet combination is added

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,39 +1,60 @@
 #!/bin/bash
-hh=0
-tt=0
-ht=0
-th=0
+hhh=0
+ttt=0
+hth=0
+tht=0
+hht=0
+thh=0
+tth=0
+htt=0
 read -p "enter the  number of tym you want to flip a coin" num
-
-#uc4
 declare -A coin
 for((i=1;i<num;i++))
 do
-         no=$((RANDOM%4))
+         no=$((RANDOM%8))
         if [ $no -eq 1 ]
         then
-        (( hh  ++ ))
-        combination=HH
+        (( hhh  ++ ))
+        combination=HHH
         elif [ $no -eq 2 ]
         then
-        (( tt  ++ ))
-        combination=TT
+        (( ttt  ++ ))
+        combination=TTT
         elif [ $no -eq 3 ]
         then
-        (( ht  ++ ))
-        combination=HT
+        (( hth  ++ ))
+        combination=HTH
+         elif [ $no -eq 4 ]
+        then
+        (( tht  ++ ))
+        combination=THT
+        elif [ $no -eq 5 ]
+        then
+        (( hht  ++ ))
+        combination=HHT
+         elif [ $no -eq 6 ]
+        then
+        (( tth  ++ ))
+        combination=TTH
+
+        elif [ $no -eq 7 ]
+        then
+        (( htt  ++ ))
+        combination=HTT
         else
-        (( th  ++ ))
-        combination=TH
-        fi
+        (( thh  ++ ))
+        combination=THH
+ fi
         coin[$i]="$combination"
 done
 echo "Dictionary : ${coin[@]}"
-echo "percentage of HH: " $((( hh * 100 ) / num ))"%"
-echo "percentage of TT: " $((( tt * 100 ) / num ))"%"
-echo "percentage of HT: " $((( ht * 100 ) / num ))"%"
-echo "percentage of TH: " $((( th * 100 ) / num ))"%"
-
-
+echo "percentage of HHH: " $((( hhh * 100 ) / num ))"%"
+echo "percentage of HHT: " $((( hht * 100 ) / num ))"%"
+echo "percentage of HTH: " $((( hth * 100 ) / num ))"%"
+echo "percentage of HTT: " $((( htt * 100 ) / num ))"%"
+echo "percentage of TTT: " $((( ttt * 100 ) / num ))"%"
+echo "percentage of TTH: " $((( tth * 100 ) / num ))"%"
+echo "percentage of THT: " $((( tht * 100 ) / num ))"%"
+echo "percentage of THH: " $((( thh * 100 ) / num ))"%"
 
 
