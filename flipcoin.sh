f0c7c9f83d034ab1c6e95d37fac301f7ad5ab4df
pratiksha1234118 <pratikshagavande1699@gmail.com>
2020-07-08 12:07:26 +0530

    Added :Singlet combination is added

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,11 +1,28 @@
-echo "coin flip combination problem"
+#!/bin/bash
+heads=0
+tails=0
+read -p "enter the  number of tym you want to flip a coin" num
+declare -A coin
 heads=0
 tails=0
 
- no=$((RANDOM%2))
- if [ $no -eq 1 ]
-  then
-	echo "head win $no"
- else
-	echo "tails win $no"
-  fi
+for((i=1;i<num;i++))
+do
+        no=$((RANDOM%2))
+        if [ $no -eq 1 ]
+        then
+                combination=H
+                (( heads ++ ))
+        else
+                 combination=T
+                (( tails++ ))
+          fi
+        coin[$i]=$combination
+done
+echo ${coin[@]}
+echo "percentage of heads: " $((( heads * 100 ) / num ))"%"
+echo "percentage of tails: " $((( tails * 100 ) / num ))"%"
+
+
+
+
